de1d114ac0e0c4ff8fcc91da39a8e8c02e5564f2
yhuzued <yusufmait@gmail.com>
2023-07-13 13:21:29 +0700

    Add conditional to return no task component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Form from "./components/Form";
 import { useEffect, useState } from "react";
 import Title from "./components/Title";
 import TodoList from "./components/TodoList";
+import EmptyTask from "./components/EmptyTask";
 
 function App() {
   const [todo, setTodo] = useState({});
@@ -11,15 +12,22 @@ function App() {
     return initialValue || [];
   });
 
+  const emptyTask = () => {
+    if (tasks.length === 0) {
+      return <EmptyTask />;
+    }
+  };
+
   useEffect(() => {
     localStorage.setItem("TODO", JSON.stringify(tasks));
   }, [tasks]);
 
   return (
     <section className="flex justify-center">
-      <main className="bg-white w-full md:w-1/2 lg:w-1/2 m-5 p-5 rounded-sm drop-shadow-md">
+      <main className="bg-white w-4/5 md:w-2/4 lg:w-1/3 m-5 p-5 rounded-sm drop-shadow-md">
         <Form todo={todo} setTodo={setTodo} tasks={tasks} setTasks={setTasks} />
         <Title />
+        {emptyTask()}
         <TodoList tasks={tasks} setTasks={setTasks} />
       </main>
     </section>
