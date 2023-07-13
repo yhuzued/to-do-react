3cb503cb767487b5f1a0e5ba543521f4048f8949
yhuzued <yusufmait@gmail.com>
2023-07-13 13:05:03 +0700

    implement helper delet todo and mark todo

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,34 +1,52 @@
 import PropTypes from "prop-types";
 
 TodoList.propTypes = {
-  tasks: PropTypes.array.isRequired,
   todo: PropTypes.object.isRequired,
+  setTodo: PropTypes.func.isRequired,
+  tasks: PropTypes.array.isRequired,
+  setTasks: PropTypes.func.isRequired,
 };
 
-function TodoList({ tasks, todo }) {
-  function deleteTodo() {
-    console.log(todo);
+function TodoList({ tasks, setTasks }) {
+  function deleteTodo(e) {
+    const target = e.target.parentNode.getAttribute("id");
+    const newList = tasks.filter((t) => t.id !== target);
+    setTasks(newList);
+  }
+
+  function taskCompleted(e) {
+    const target = e.target.parentNode.getAttribute("id");
+    const newList = tasks.map((t) => {
+      if (t.id === target) {
+        t.completed = !t.completed;
+      }
+      return t;
+    });
+    setTasks(newList);
   }
 
   const list = tasks.map((t) => {
     return (
       <li
         key={t.id}
+        id={t.id}
         className="flex justify-between gap-1 mx-3 my-2 align-center bg-gray-50 hover:bg-gray-100 items-center p-3"
       >
         <input
           type="checkbox"
-          id={t.id + "checkbox"}
+          id={t.id + "-checkbox"}
           className="w-6 h-6 peer"
+          onChange={taskCompleted}
+          checked={t.completed}
         />
         <label
-          htmlFor={t.id + "checkbox"}
+          htmlFor={t.id + "-checkbox"}
           className="w-full p-1 text-lg peer-checked:line-through peer-checked:text-slate-400"
         >
           {t.task}
         </label>
         <button
-          className="bg-red-500 px-2 py-1 rounded-sm text-gray-50"
+          className="bg-red-500 px-2 py-1 rounded-sm text-gray-50 hover:bg-red-700"
           onClick={deleteTodo}
         >
           Close
